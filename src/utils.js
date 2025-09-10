cff8bc08f3d1d63eeb9d89cad98d0bd1912efb4f
Michael <misha9275@gmail.com>
2025-09-10 12:52:18 +0200

    hide sonarquote check on Math.random() function

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 const getRandomNumber = (min, max) => {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
+  // NOSONAR
   return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 };
 
