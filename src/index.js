12780fc3e6b35cd26c9b3c63cf9c224f081c7938
Michael <misha9275@gmail.com>
2025-09-22 17:29:12 +0200

    fixed index.js according to fallen test

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-const roundCount = 3;
+const roundCount = 3
 
 const runEngine = (rule, getQuestionAndAnswer) => {
-  console.log('Welcome to the Brain Games!');
-  console.log(rule);
+  console.log('Welcome to the Brain Games!')
+  console.log(rule)
 
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
 
   for (let i = 0; i < roundCount; i += 1) {
-    const [question, correctAnswer] = getQuestionAndAnswer();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    const [question, correctAnswer] = getQuestionAndAnswer()
+    console.log(`Question: ${question}`)
+    const userAnswer = readlineSync.question('Your answer: ')
 
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log (`Let's try again, ${name}!`);
-      return;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log (`Let's try again, ${name}!`)
+      return
     }
 
-    console.log('Correct!');
+    console.log('Correct!')
   }
 
-  console.log(`Congratulations, ${name}!`);
-};
+  console.log(`Congratulations, ${name}!`)
+}
 
-export default runEngine;
+export default runEngine
